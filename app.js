a9db8e0e001d9113a8d7e4d90582c980817b90dc
Pranay Singh <sng.pranay@gmail.com>
2016-02-21 18:08:04 -0500

    front end server

---
+++ app.js
@@ -0,0 +1,43 @@
+var application_root = __dirname;
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);   
+var path = require("path");
+var request = require("request");
+
+
+app.use('/', express.static(__dirname + '/public'));
+
+
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
+});
+
+//Launch serverindex
+
+http.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
+
+io.on('connection', function(socket){
+  console.log('a client connected');
+  socket.on('file',function(file){
+  	console.log(file);
+  	var query = 'http://www.LocationOfServer.com' + '?' + 'Service=' + file.Service + '&' + 'line=' + file.Line + '&' + 'Start=' + file.Start + '&' + 'End=' +file.end ;
+  	console.log(query); 
+  	request(query, function (error, response, body) { //might need to type cast query as string as requst is expecting a string
+    if (!error && response.statusCode == 200) {
+        console.log(body); 
+    }
+		});
+	});
+});
+
+// request('http://www.modulus.io', function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//         console.log(body); 
+//     }
+// });
