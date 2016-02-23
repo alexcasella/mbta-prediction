1d767ceb62ef5aa928c639585089bf156df9484f
Pranay Singh <sng.pranay@gmail.com>
2016-02-22 23:32:56 -0500

    changed query string according to Java API

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ io.on('connection', function(socket){
   console.log('a client connected');
   socket.on('file',function(file){
   	console.log(file);
-  	var query = 'http://www.LocationOfServer.com' + '?' + 'Service=' + file.Service + '&' + 'line=' + file.Line + '&' + 'Start=' + file.Start + '&' + 'End=' +file.end ;
+  	var query = 'http://www.LocationOfServer.com' + '/' + file.Service + '/' + file.Line + '/' + file.Start 
   	console.log(query); 
   	request(query, function (error, response, body) { //might need to type cast query as string as requst is expecting a string
     if (!error && response.statusCode == 200) {
