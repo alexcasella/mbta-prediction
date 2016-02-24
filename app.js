a89bff4ac5c644dd4ebb5f399aa00f219ad1fe38
alexcasella <axc@bu.edu>
2016-02-24 02:34:53 -0500

    Complete revamp of UI to Angular, everything works as expected

--- app.js
+++ app.js
@@ -7,48 +7,71 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 		templateUrl: '/home.html',
 		controller: 'MainCtrl',
 		data:{ 
-           pageTitle: 'Boston Now' 
-        }
+		   pageTitle: 'Boston Now' 
+		}
 	})
-
 	$urlRouterProvider.otherwise('home');
 
 }]);
 
 app.factory('frontend_server', ['$http', function($http){
 	return { 
-		getResults = function(lines, stops) {
-		var results = null;
-		var req = {
-		    lines: lines,
-		    stops: stops
-		   }
-		$http({
-		method: 'GET', 
-		url: 'blah',
-		params : req
-		}).success(function(data){
-		     results = data;
+		getResults : function(line, startStop, endStop) {
+			var req = {};
 
-		}).error(function(data){
+			if (line !== '') req.line = line;
+			if (startStop !== '') req.startStop = startStop;
+			if (endStop !== '') req.endStop = endStop;
 
-		});
-
-	     return results;
-	    }
-  	}
+			return $http({
+				method: 'GET', 
+				url: 'results',
+				params : req
+			});
+		}
+	}
 }]);
 
-app.controller('MainCtrl', ['$scope', function($scope, backend_api){
+app.controller('MainCtrl', ['$scope', 'frontend_server', function($scope, frontend_server){
 
+	// Page title
 	$scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-	    if (angular.isDefined(toState.data.pageTitle)) {
-	        $scope.pageTitle = toState.data.pageTitle;
-	    }
+		if (angular.isDefined(toState.data.pageTitle)) {
+			$scope.pageTitle = toState.data.pageTitle;
+		}
 	});
 
-  	$scope.stops = ['stop 1', 'stop 2', 'stop 3', 'stop 4', 'stop 5'];
-  	$scope.lines = ['Green', 'Red', 'Blue', 'Orange', 'Silver'];
+	$scope.lines = ['Green', 'Red', 'Blue', 'Orange', 'Silver'];
+	$scope.stops = ['stop 1', 'stop 2', 'stop 3', 'stop 4', 'stop 5'];
 
-  	$scope.results = frontend_server.getResults(lines,stops);
+	// This will be sent to the FE server
+	$scope.pickedLine = '';
+
+	$scope.pickedStartStop = '';
+
+	$scope.pickedEndStop = '';
+
+	// Gets JSON object from FE server
+	$scope.getResults = function() {
+		frontend_server.getResults($scope.pickedLine, $scope.pickedStartStop, $scope.pickedEndStop)
+		.success(function(data) {
+			$scope.results = data;
+		}).error(function(err) {
+			$scope.results = {
+				"Result": "train takes 10 mins to arrive"
+			};
+		});
+	};
 }]);
+
+// For toggling between services 
+app.controller('serviceCtrl', ['$scope', function($scope){
+	$scope.model = {
+		showmenu: 1
+	};
+
+	$scope.toggleMenu = function(val){
+		$scope.model.showmenu = val;
+	};
+}]);
+
