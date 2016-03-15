6d253593ec53a5973891ae5bdace13327f93a684
Pranay Singh <sng.pranay@gmail.com>
2016-03-15 17:48:57 -0400

    Front end connect to backend II

--- app.js
+++ app.js
@@ -26,11 +26,12 @@ io.on('connection', function(socket){
   console.log('a client connected');
   socket.on('file',function(file){
   	console.log(file);
-  	var query = 'http://www.LocationOfServer.com' + '/' + file.Service + '/' + file.Line + '/' + file.Start 
+  	var query = 'http://messenger-env.us-west-2.elasticbeanstalk.com/webapi/messages/greenline/' + file.Start 
   	console.log(query); 
   	request(query, function (error, response, body) { //might need to type cast query as string as requst is expecting a string
     if (!error && response.statusCode == 200) {
         console.log(body); 
+        socket.emit('body', body);
     }
 		});
 	});
