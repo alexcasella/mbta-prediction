4fcd5e2b391c4858e3c3c385b0fad5906beac0f0
AndyLi07 <angli@bu.edu>
2016-04-22 21:57:48 -0400

    calculate distance

--- DynamoDB_Demo/src/com/mbta/util/ConstDefine.java
+++ DynamoDB_Demo/src/com/mbta/util/ConstDefine.java
@@ -3,8 +3,16 @@ package com.mbta.util;
 public class ConstDefine {
 
 	public static final String mbtaBaseURI = "http://realtime.mbta.com/developer/api/v2/";
-	
+
 	public static final String apiKey = "?api_key=TGmTHy9PgES2eVZlZ76Mjg&";
-	
+
+	public static final String apiKey2 = "?api_key=jj9cgR_zj0us-aQ7Dn-3IQ&";
+
 	public static final String format = "&format=json";
+
+	public static double WALKABLE_DISTANCE = 0.001;
+
+	public static double WALK_SPEED = 1.0;
+
+	public static double TRAIN_SPEED = 20.0;
 }
